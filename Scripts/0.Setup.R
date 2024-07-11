25230423065b90c4b4e24a1e3e916cd12a330f37
Marcel Muller <102991415+MarcelCM@users.noreply.github.com>
2024-07-11 15:22:45 +0200

    MM_Setup_Paths_Update

--- Scripts/0.Setup.R
+++ Scripts/0.Setup.R
@@ -109,13 +109,13 @@ if (Sys.getenv("USERNAME") == "WRQ") {
   
 } else if (Sys.getenv("USERNAME") == "R5532132") { # Marcel
   # - Custom path where R-scripts are saved
-  path_cust <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classifier-Diagnostics/Scripts/"
+  path_cust <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classfier-Diagnostics Repository/Classifier-Diagnostics/Scripts/"
   
   # - Common path for storing important R-objects as back-up
-  genObjPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classifier-Diagnostics/Objects/"
+  genObjPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classfier-Diagnostics Repository/Classifier-Diagnostics/Objects/"
   
   # - Common path for saving important analytics (e.g., sampling)
-  genFigPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classifier-Diagnostics/Figures/"
+  genFigPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classfier-Diagnostics Repository/Classifier-Diagnostics/Figures/"
   
 } else if (Sys.getenv("USERNAME") == "R5668395") { # Roland
   # - Custom path where R-scripts are saved
