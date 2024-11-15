a7199e4bb67c27a9fa455ee75918f2582b10517c
Dr Arno Botha <arno.spasie.botha@gmail.com>
2024-11-15 15:49:35 +0200

    Housekeeping
    Given changes in machines, as well as the recent R-upgrade, I did some housekeeping for creating new paths for my usename(s) (machine-dependent) at least.

--- Scripts/0.Setup.R
+++ Scripts/0.Setup.R
@@ -70,63 +70,53 @@ d <- 3 # default threshold for g0/g1-measures of delinquency (payments in arrear
 k <- 6 # Probation period
 
 
-# -- Path variables | General
-
-# - Common path for saving big data objects
-genPath <- "C:/Data/Classifier-Diagnostics_Data/"
-
-# - Common path for importing raw data
-genRawPath <- "C:/Data/"
-
-
 # -- Path variables | User-dependent
 
-if (Sys.getenv("USERNAME") == "WRQ") {
-  # - Custom path where R-scripts are saved
-  path_cust <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Classifier-Diagnostics/Scripts/"
-  
-  # - Common path for storing important R-objects as back-up
-  genObjPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Classifier-Diagnostics/Objects/"
-  
-  # - Common path for saving important analytics (e.g., sampling)
-  genFigPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Classifier-Diagnostics/Figures/"
-  
+if (Sys.getenv("USERNAME") == "u15247199") { # Dr Arno Botha | Tuks-machine
+  # - Common path for saving large R-objects as back-up and/or as reusable checkpoints
+  genPath <- "C:/DataDump/RetailMortgages-FNB/"
+  # - Common path from which raw big datasets are imported
+  genRawPath <- "C:/DataDump/RetailMortgages-FNB/Classifier-Diagnostics_Data/"
+  # - Common path for sourcing R-scripts in main codebase
+  path_cust <- "C:/Users/u15247199/Google Drive/My Drive/WorkLife/Analytix/R&D Codebases/Classifier-Diagnostics/Scripts/"
+  # - Common path for storing important (but small!) R-objects as back-up
+  genObjPath <- "C:/Users/u15247199/Google Drive/My Drive/WorkLife/Analytix/R&D Codebases/Classifier-Diagnostics/Objects/"
+  # - Common path for saving important analytics and figures
+  genFigPath <- "C:/Users/u15247199/Google Drive/My Drive/WorkLife/Analytix/R&D Codebases/Classifier-Diagnostics/Figures/"
   # --- Custom path where R-scripts are saved
-} else if (Sys.getenv("USERNAME") == "Arno Botha") {
-  path_cust <- "E:/WorkLife/Analytix/Research/Classifier-Diagnostics/Scripts/"
-  
-  # - Common path for storing important R-objects as back-up
-  genObjPath <- "E:/WorkLife/Analytix/Research/Classifier-Diagnostics/Objects/"
-  
-  # - Common path for saving important analytics (e.g., sampling)
-  genFigPath <- "E:/WorkLife/Analytix/Research/Classifier-Diagnostics/Figures/"
-  
-  # - Common path for saving big data objects
+} else if (Sys.getenv("USERNAME") == "Arno Botha") { # Dr Arno Botha | Kralkatorrik-machine
+  # - Common path for saving large R-objects as back-up and/or as reusable checkpoints
   genPath <- "E:/DataDump/FNB SLC/Classifier-Diagnostics_Data/"
-  
-  # - Common path for importing raw data
+  # - Common path from which raw big datasets are imported
   genRawPath <- "E:/DataDump/FNB SLC/"
-  
-} else if (Sys.getenv("USERNAME") == "R5532132") { # Marcel
-  # - Custom path where R-scripts are saved
+  # - Common path for sourcing R-scripts in main codebase
+  path_cust <- "E:/WorkLife/Analytix/Research/Classifier-Diagnostics/Scripts/"
+  # - Common path for storing important (but small!) R-objects as back-up
+  genObjPath <- "E:/WorkLife/Analytix/Research/Classifier-Diagnostics/Objects/"
+  # - Common path for saving important analytics and figures
+  genFigPath <- "E:/WorkLife/Analytix/Research/Classifier-Diagnostics/Figures/"
+} else if (Sys.getenv("USERNAME") == "R5532132") { # Marcel Muller | Botha-machine
+  # - Common path for saving large R-objects as back-up and/or as reusable checkpoints
+  genPath <- "C:/Data/Classifier-Diagnostics_Data/"
+  # - Common path from which raw big datasets are imported
+  genRawPath <- "C:/Data/"
+  # - Common path for sourcing R-scripts in main codebase
   path_cust <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classfier-Diagnostics Repository/Classifier-Diagnostics/Scripts/"
-  
-  # - Common path for storing important R-objects as back-up
+  # - Common path for storing important (but small!) R-objects as back-up
   genObjPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classfier-Diagnostics Repository/Classifier-Diagnostics/Objects/"
-  
-  # - Common path for saving important analytics (e.g., sampling)
+  # - Common path for saving important analytics and figures
   genFigPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Classfier-Diagnostics Repository/Classifier-Diagnostics/Figures/"
-  
-} else if (Sys.getenv("USERNAME") == "R5668395") { # Roland
-  # - Custom path where R-scripts are saved
+} else if (Sys.getenv("USERNAME") == "R5668395") { # Roland Roland | Botha-machine
+  # - Common path for saving large R-objects as back-up and/or as reusable checkpoints
+  genPath <- "C:/Data/Classifier-Diagnostics_Data/"
+  # - Common path from which raw big datasets are imported
+  genRawPath <- "C:/Data/"
+  # - Common path for sourcing R-scripts in main codebase
   path_cust <- "C:/Users/R5668395/Documents/GitHub ClassDiag/Classifier-Diagnostics/Scripts/"
-  
-  # - Common path for storing important R-objects as back-up
+  # - Common path for storing important (but small!) R-objects as back-up
   genObjPath <- "C:/Users/R5668395/Documents/GitHub ClassDiag/Classifier-Diagnostics/Objects/"
-  
-  # - Common path for saving important analytics (e.g., sampling)
+  # - Common path for saving important analytics and figures
   genFigPath <- "C:/Users/R5668395/Documents/GitHub ClassDiag/Classifier-Diagnostics/Figures/"
-  
 } else {
     stop("User-specific paths not set for current user: ", Sys.getenv("USERNAME"), ". Please fix in Setup script (0.Setup.R) before continuing")
 }
